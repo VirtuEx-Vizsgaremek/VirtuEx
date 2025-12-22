50cab769dc11c4d0141023569cb8ca46cfb3ccdd
TheClashFruit <55049569+TheClashFruit@users.noreply.github.com>
2025-12-22 11:14:34 +0100

    feat: setup api

---
+++ apps/api/src/mikro-orm.config.ts
@@ -0,0 +1,45 @@
+import {
+  Options,
+  PostgreSqlDriver,
+  DefaultLogger,
+  LoggerNamespace,
+  LogContext
+} from '@mikro-orm/postgresql';
+import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
+
+import Logger from '@/util/logger';
+
+class CustomLogger extends DefaultLogger {
+  log(namespace: LoggerNamespace, message: string, context?: LogContext) {
+    const logger = new Logger(`virtuex::api::orm::${namespace}`);
+    // Create your own implementation for output:
+
+    switch ((context || { level: 'info' }).level) {
+      case 'info':
+        logger.info(message);
+        break;
+      case 'error':
+        logger.error(message);
+        break;
+      case 'warning':
+        logger.warn(message);
+        break;
+    }
+  }
+}
+
+const config: Options = {
+  driver: PostgreSqlDriver,
+  dbName: process.env.DB_NAME || 'postgres',
+  user: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASS || 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432'),
+  entities: ['build/**/*.entity.js'],
+  entitiesTs: ['src/**/*.entity.ts'],
+  debug: process.env.NODE_ENV !== 'production',
+  loggerFactory: (options) => new CustomLogger(options),
+  highlighter: new SqlHighlighter()
+};
+
+export default config;
