99630c711528087d69619309d1fad9cacf32cf1c
herben <heredibenedek@gmail.com>
2025-12-23 11:05:25 +0100

    feat: planmod more options

--- apps/web/src/app/profile/page.tsx
+++ apps/web/src/app/profile/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import SideNav from "@/components/ui/sidenav";
+import { ModifyPlanModal } from "@/components/ui/planmod";
 
 import {
   Item,
@@ -200,14 +201,9 @@ export default function ProfilePage() {
                                     ))}
                                 </div>
 
+
                                 <CardFooter className="flex justify-center mt-auto pt-8 px-0">
-                                    <Button 
-                                    variant="default" 
-                                    className="w-full sm:w-auto bg-gray-900 hover:bg-black text-white px-10 py-6 text-lg font-semibold transition-all shadow-md"
-                                    
-                                    >
-                                    Modify
-                                    </Button>
+                                    <ModifyPlanModal currentCredits={user[0].credits} currentPlan={user[0].plan} />
                                 </CardFooter>
                                 
                                 </div>
