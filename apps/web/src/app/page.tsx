92e0ea270888fade53f3a6edb208a858e412d93a
HaxMaster <120025122+ItsMeHaxMaster@users.noreply.github.com>
2025-12-23 12:17:49 +0100

    feat: redesigned navbar, uploaded all logo variants, added theme changer button

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import {
@@ -20,32 +22,54 @@ import {
   CardHeader,
   CardTitle
 } from '@/components/ui/card';
+import { Moon, Sun } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    if (isDark) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [isDark]);
+
   return (
-    <div>
-      <div className="flex justify-between gap-4 m-auto mt-4 p-2">
-        <NavigationMenu className="w-full">
-          <NavigationMenuList className="w-full">
-            <div className="flex justify-self-start gap-4">
+    <div className="min-h-screen">
+      <nav className="bg-card border-b border-border shadow-sm">
+        <div className="flex justify-between gap-4 m-auto max-w-[85%] px-4 py-3 items-center">
+          <NavigationMenu>
+            <NavigationMenuList className="gap-6">
               <NavigationMenuItem>
-                <NavigationMenuLink>
+                <NavigationMenuLink className="text-foreground hover:text-primary transition-colors">
                   <Link href="">Premium</Link>
                 </NavigationMenuLink>
               </NavigationMenuItem>
               <NavigationMenuItem>
-                <NavigationMenuLink>
+                <NavigationMenuLink className="text-foreground hover:text-primary transition-colors">
                   <Link href="">About Us</Link>
                 </NavigationMenuLink>
               </NavigationMenuItem>
-            </div>
-          </NavigationMenuList>
-        </NavigationMenu>
-        <div className="flex justify-self-end gap-4">
-          <Button>Sign Up</Button>
-          <Button>Log In</Button>
+            </NavigationMenuList>
+          </NavigationMenu>
+
+          <img src="VirtuEx_logo-bg-gl.svg" alt="Logo" className="h-15" />
+
+          <div className="flex gap-3">
+            <Button variant="outline">Sign Up</Button>
+            <Button>Log In</Button>
+            <Button
+              size="icon"
+              variant="ghost"
+              onClick={() => setIsDark(!isDark)}
+            >
+              {isDark ? <Sun /> : <Moon />}
+            </Button>
+          </div>
         </div>
-      </div>
+      </nav>
     </div>
   );
 }
