5f684649cbae505b6b2d1c03a359b4ea516732a2
TheClashFruit <clash.fruit123@gmail.com>
2026-01-28 19:34:50 +0100

    feat: update user details

--- apps/api/src/routes/v1/user/@me/index.ts
+++ apps/api/src/routes/v1/user/@me/index.ts
@@ -2,6 +2,7 @@ import { User } from '@/entities/user.entity';
 import Status from '@/enum/status';
 
 import { Request, Response } from '@/util/handler';
+import { orm } from '@/util/orm';
 import z from 'zod';
 
 export const schemas = {
@@ -17,6 +18,14 @@ export const schemas = {
       permissions: z.bigint(),
       subscription: z.string(),
       activated: z.boolean()
+    })
+  },
+  patch: {
+    req: z.object({
+      username: z.string().optional(),
+      full_name: z.string().optional(),
+      email: z.email().optional(),
+      bio: z.string().nullable().optional()
     })
   }
 };
@@ -41,8 +50,35 @@ export const get = async (
   });
 };
 
-export const patch = (req: Request, res: Response<string>) => {
-  res.status(Status.Ok).send('Hello, World!');
+export const patch = async (req: Request, res: Response<string>) => {
+  const user = await req.getUser();
+  const db = (await orm).em.fork();
+
+  const {
+    email,
+    username,
+    full_name: fullName,
+    bio
+  } = req.validateBody(schemas.patch.req);
+
+  try {
+    if (email) user.email = email;
+    if (username) user.username = username;
+    if (fullName) user.fullName = fullName;
+    if (bio !== undefined) user.bio = bio;
+
+    await db.persist(user).flush();
+
+    res.status(Status.NoContent).end();
+  } catch (e: any) {
+    if (e.name === 'UniqueConstraintViolationException')
+      return res.error(
+        Status.Conflict,
+        'A user with this username already exists.'
+      );
+
+    throw e;
+  }
 };
 
 export const del = (req: Request, res: Response<string>) => {
