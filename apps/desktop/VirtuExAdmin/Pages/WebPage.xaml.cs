a3ae856cc5798fa9c3ba47ccabd4fcebb14556ec
TheClashFruit <admin@theclashfruit.me>
2026-03-27 19:47:08 +0100

    feat: magic login to webview

--- apps/desktop/VirtuExAdmin/Pages/WebPage.xaml.cs
+++ apps/desktop/VirtuExAdmin/Pages/WebPage.xaml.cs
@@ -6,19 +6,26 @@ using VirtuExAdmin.Util;
 namespace VirtuExAdmin.Pages;
 
 public partial class WebPage : Page {
-    public WebPage() {
+    private readonly ApiClient _api;
+    
+    public WebPage(ApiClient api) {
+        _api = api;
+        
         InitializeComponent();
         
         WebView.DefaultBackgroundColor =  Color.Transparent;
+
+        LoadWebview();
+    }
+
+    async void LoadWebview() {
+        await WebView.EnsureCoreWebView2Async(null);
         
-        /*
-        WebView.Initialized += (sender, args) => {
-            // Authenticate the user automagically.
-            var cookie = WebView.CoreWebView2.CookieManager.CreateCookie("vtx_token", ApiClient.Instance.Token, "legany.hu", "/");
-            cookie.IsHttpOnly = false;
-            cookie.IsSecure   = true;
-            WebView.CoreWebView2.CookieManager.AddOrUpdateCookie(cookie);
-        };
-        */
+        var cookie = WebView.CoreWebView2.CookieManager.CreateCookie("vtx_token", _api.Token, "localhost", "/");
+        cookie.Expires  = DateTimeOffset.Now.AddMonths(1).DateTime;
+        cookie.SameSite = CoreWebView2CookieSameSiteKind.Lax;
+        cookie.IsSecure = false;
+            
+        WebView.CoreWebView2.CookieManager.AddOrUpdateCookie(cookie);
     }
 }
